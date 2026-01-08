d504e6f3b3eab01df9c49f64660102362014dd14
WhichChapter <camelstoe69@gmail.com>
2026-01-08 11:05:14 +0000

    Update new.lua

--- guis/new.lua
+++ guis/new.lua
@@ -551,24 +551,24 @@ local function hookCF(func, settings)
 				if shared.VoidDev then
 					task.spawn(function()
 						repeat task.wait() until errorNotification ~= nil and type(errorNotification) == "function"
-						errorNotification("Voidware | "..tostring(S_Name), debug.traceback(tostring(err)), 10)
+						errorNotification("skidware | "..tostring(S_Name), debug.traceback(tostring(err)), 10)
 					end)
 				end
 				task.spawn(function()
 					repeat task.wait() until errorNotification ~= nil and type(errorNotification) == "function"
 					if S_Name ~= "Not Specified" then
 						if attemptedRestarts[S_Name] then 
-							errorNotification('Voidware | '..tostring(S_Name), "Restart failed!", 3)
-							errorNotification("Voidware | "..tostring(S_Name), "There was an error with this module. If you can please send the\n VW_Error_Log.json in your workspace to erchodev#0 or discord.gg/voidware", 10)
+							errorNotification('skidware | '..tostring(S_Name), "Restart failed!", 3)
+							errorNotification("skidware | "..tostring(S_Name), "There was an error with this module. If you can please send the\n VW_Error_Log.json in your workspace to erchodev#0 or discord.gg/skidware", 10)
 						else
-							errorNotification('Voidware | '..tostring(S_Name), "There was an error with this module. Attempting restart...", 3)
+							errorNotification('skidware | '..tostring(S_Name), "There was an error with this module. Attempting restart...", 3)
 							attemptedRestarts[S_Name] = true
 							local suc2, err2 = pcall(function() func(false) end)
-							if suc2 then InfoNotification("Voidware | "..tostring(S_Name), "Restart successfull!", 3)
+							if suc2 then InfoNotification("skidware | "..tostring(S_Name), "Restart successfull!", 3)
 end
 						end
 					else
-						errorNotification("Voidware | "..tostring(S_Name), "There was an error with this module. If you can please send the\n VW_Error_Log.json in your workspace to erchodev#0 or discord.gg/voidware", 10)
+						errorNotification("skidware | "..tostring(S_Name), "There was an error with this module. If you can please send the\n VW_Error_Log.json in your workspace to erchodev#0 or discord.gg/skidware", 10)
 					end
 				end)
 				local errorLog = {
@@ -5647,7 +5647,7 @@ function mainapi:Load(skipgui, profile)
 						task.spawn(function()
 							repeat task.wait() until errorNotification ~= nil and type(errorNotification) == "function" 
 							pcall(function()
-								errorNotification("Voidware", "Failure loading "..tostring(v).." Error: "..tostring(err), 5)
+								errorNotification("skidware", "Failure loading "..tostring(v).." Error: "..tostring(err), 5)
 							end)
 						end)
 					end
@@ -5719,7 +5719,7 @@ end
 local function notifyError(err)
 	if errorNotification ~= nil and type(errorNotification) == "function" then
 		pcall(function()
-			errorNotification("Voidware", tostring(err), 5)
+			errorNotification("skidware", tostring(err), 5)
 		end)
 	end
 end
